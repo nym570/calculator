d460d61845ec31f5af6acbf597d4e6a4e0a9f508
nym570 <lailifr@student.ub.ac.id>
2019-10-26 10:30:14 +0700

    tambah penjumlahan

--- src/abc/Abc.java
+++ src/abc/Abc.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abc;
-
-/**
- *
- * @author lili
- */
 public class Abc {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int firstNumber = 10,secondNumber =10;
+        System.out.println(firstNumber+secondNumber);  
     }
     
 }
